3dfdf94a3cc328148843fdf67bc16e4bd2e7ec36
dzianismironau <denis.mironow00@gmail.com>
2024-05-13 17:12:49 +0200

    2024-05-13 DM

--- app/routes.py
+++ app/routes.py
@@ -1,13 +1,32 @@
 from app import app
-from flask import render_template
+from flask import render_template, request, redirect, url_for
+from bs4 import BeautifulSoup
+import requests
 
 @app.route('/')
 @app.route('/index')
 def index():
     return render_template("index.html")
 
-@app.route('/extract')
-def index():
+@app.route('/extract', methods=['POST', 'GET'])
+def extract():
+    if request.method == 'POST':
+        product_id = request.form.get('product_id')
+        url = f'https://www.ceneo.pl/{product_id}' 
+        response = requests.get(url)
+        if response.status_code == requests.codes['ok']:
+            page_dom = BeautifulSoup(response.text, 'html.parser')
+            try:
+                opinions_count = page_dom.select_one("a.product-review__link > span").get_text().strip()
+            except AttributeError:
+                opinions_count = 0
+            if opinions_count:
+                # proces ekstrakcji
+                return redirect(url_for('product', product_id=product_id))
+            error = "Brak opinii."
+            return render_template('extract.html', error=error)
+        error = "Błędny kod."
+        return render_template('extract.html', error=error)
     return render_template("extract.html")
 
 @app.route('/products')
@@ -21,7 +40,3 @@ def author():
 @app.route('/product/<product_id>')
 def product(product_id):
     return render_template("product.html", product_id=product_id)
-
-@app.route('/hello/<name>')
-def hello(name="World"):
-    return f"Hello, {name}!"
